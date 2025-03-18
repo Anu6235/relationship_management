c7f8d4cd6e659a952e7d779d02950fdfe635ba52
Anusree <sajeevananusree68@gmail.com>
2025-03-18 12:35:12 +0530

    Refactor static file serving and file upload directory path for improved clarity

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
- const threeFoldersBack = path.resolve(__dirname, '../');
-app.use(express.static(path.join(threeFoldersBack, 'public')));
+//  const threeFoldersBack = path.resolve(__dirname, '../');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // API Routes
 app.use('/api/auth', authRoutes);
