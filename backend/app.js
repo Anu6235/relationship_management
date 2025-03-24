029896dcd14c9df75ceaea430c19d33059b806c7
Anusree <sajeevananusree68@gmail.com>
2025-03-24 12:14:34 +0530

    refactor: streamline middleware and API route setup in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,32 @@
 const express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors');
+
+require('dotenv').config();
+
 const authRoutes = require('./routes/authRoutes');
 const memberRoutes = require('./routes/memberRoutes');
 const appConfigRouter = require('./routes/appConfig');
 const ledgerRoutes = require('./routes/ledgerRoutes');
 const ledgerTypeRoutes = require('./routes/ledgerTypeRoutes');
 
-require('dotenv').config();
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 const app = express();
 
+// Middleware setup
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
-app.use('/users', usersRouter);
+// API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/members', memberRoutes);
 app.use('/api/app-config', appConfigRouter);
 app.use('/api/ledgers', ledgerRoutes);
 app.use('/api/ledger-types', ledgerTypeRoutes);
-
 
 // Serve Angular frontend
 const distPath = path.join(__dirname, 'dist', 'frontend', 'browser');
@@ -43,10 +37,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
+// Error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ success: false, error: 'Something went wrong!' });
-  });
-
+});
 
 module.exports = app;
