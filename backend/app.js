ce29b7a4901b85475e80bb51b37d5497bed8b522
Anusree <sajeevananusree68@gmail.com>
2025-03-24 16:49:26 +0530

    chore: serve static files from the public directory

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//  const threeFoldersBack = path.resolve(__dirname, '../');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // API Routes
 app.use('/api/auth', authRoutes);
